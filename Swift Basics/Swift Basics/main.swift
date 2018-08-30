22811b4ce733ed08c5879b47cabf0b75312a7a52
Chris Rehagen <ccrwy5@mail.missouri.edu>
2018-08-30 15:28:27 -0500

    First commit

---
+++ Swift Basics/Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Chris Rehagen on 8/30/18.
+//  Copyright © 2018 Chris Rehagen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
